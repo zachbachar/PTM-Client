b64695eaab9b5fbba0dd4b249f6bd65ef1135dbe
dnylsgl <dnylsgl@10.0.0.27>
2018-12-18 13:14:45 +0200

    Media is played from proj

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,5 @@
 package view;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import model.PipeGameModel;
@@ -10,35 +10,34 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
 	MediaPlayer mediaPlayer;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			PipeGameModel pgm = new PipeGameModel();
 			PipeGameViewModel vm = new PipeGameViewModel(pgm);
-			
-			FXMLLoader fxl=new FXMLLoader();
-			BorderPane root= fxl.load(getClass().getResource("MainWindow.fxml").openStream());
-			MainWindowController mwc=fxl.getController(); // View
+
+			FXMLLoader fxl = new FXMLLoader();
+			BorderPane root = fxl.load(getClass().getResource("MainWindow.fxml").openStream());
+			MainWindowController mwc = fxl.getController(); // View
 			mwc.setViewModel(vm);
-			//vm.addObserver(mwc);//			mwc.playSound("BackroundTheme.wav");
-			Media musicFile = new Media("file:///Users/zachbachar/eclipse-workspace/PTM-Client/src/view/BackroundTheme.wav");
-			mediaPlayer = new MediaPlayer(musicFile);
+
+			Media audio = new Media(getClass().getResource("BackroundTheme.wav").toString());
+			mediaPlayer = new MediaPlayer(audio);
 			mediaPlayer.setAutoPlay(true);
 			mediaPlayer.setCycleCount(10);
-			
-			Scene scene = new Scene(root,400,400);
+
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
